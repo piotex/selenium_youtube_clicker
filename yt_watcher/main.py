9213323465d3cd7149f41a7abf581c3f4b498e82
piotex <pkubon2@gmail.com>
2023-06-16 22:59:41 +0200

    Update main.py

--- yt_watcher/main.py
+++ yt_watcher/main.py
@@ -10,27 +10,32 @@ hard_will___confidence_to_watch = 70
 with open(file_name) as json_file:
     data_dict = json.load(json_file)
 
-driver = get_init_driver()
+try:
+    driver = get_init_driver()
+    for i in data_dict:
+        if random.randrange(0, 100) < hard_will___confidence_to_watch:
+            elem = data_dict[i]
+            title_film = elem["title"]
+            channel_and_film_title = channel + " " + title_film
+            time_film = elem["time"].split(":")
+            time_film = int(time_film[0])*60 + int(time_film[1])
 
-for i in data_dict:
-    if random.randrange(0, 100) < hard_will___confidence_to_watch:
-        elem = data_dict[i]
-        title_film = elem["title"]
-        channel_and_film_title = channel + " " + title_film
-        time_film = elem["time"].split(":")
-        time_film = int(time_film[0])*60 + int(time_film[1])
+            search_for_my_video_in_youtube_search(driver, other_devops_films)
+            go_to_rand_film_from_search_option(driver)
 
-        search_for_my_video_in_youtube_search(driver, other_devops_films)
-        go_to_rand_film_from_search_option(driver)
-
-        search_for_my_video_in_youtube_search(driver, channel_and_film_title)
-        localized_my_film = go_to_film_from_search_option(driver, title_film, time_film)
-        if not localized_my_film:
-            go_to_film_from_profile_page(driver, title_film, time_film)
-        simulate_user_scroll(driver)
-        print("=============")
-        print(f"{i}    {title_film}")
-        print("=============")
+            search_for_my_video_in_youtube_search(driver, channel_and_film_title)
+            localized_my_film = go_to_film_from_search_option(driver, title_film, time_film)
+            if not localized_my_film:
+                go_to_film_from_profile_page(driver, title_film, time_film)
+            simulate_user_scroll(driver)
+            print("=============")
+            print(f"{i}    {title_film}")
+            print("=============") 
+except Exception as eee:
+    print("======ERROR=======")
+    print(eee)
+    print("=============")
+    pass
 
 time.sleep(1)
 print("====== END =======")
