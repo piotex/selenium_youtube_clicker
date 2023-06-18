9febb78de07453e84b583efcde60593c99ba5cb5
Piotr Kubon_devC <pkubon2@gmail.com>
2023-06-19 00:01:47 +0200

    watcher changes

--- yt_watcher/main.py
+++ yt_watcher/main.py
@@ -11,37 +11,23 @@ def get_title_time(data_dict, i):
     return title_film, time_film
 
 
-def main(confidenceee, other_film_sleep_time):
+def main(my_confidenceee, other_film_sleep_time):
     file_name = 'data.json'
     other_devops_films = "dev ops for beginners"
     channel = "Piotr Kubon Dev Ops AI"
-    hard_will___confidence_to_watch = confidenceee
-
-    sys.stdout.write(f"\r\n   000    \r\n")
+    hard_will___confidence_to_watch = my_confidenceee
 
     with open(file_name) as json_file:
         data_dict = json.load(json_file)
 
-    sys.stdout.write(f"\r\n   001    \r\n")
-    sys.stdout.write(f"\r\n   {data_dict}    \r\n")
-
     try:
-        sys.stdout.write(f"\r\n   002    \r\n")
         driver = get_init_driver()
-        sys.stdout.write(f"\r\n   003    \r\n")
         for i in data_dict:
             title_film = ""
-            sys.stdout.write(f"\r\n   1    \r\n")
             search_for_my_video_in_youtube_search(driver, other_devops_films)
-            sys.stdout.write(f"\r\n   2    \r\n")
             go_to_rand_film_from_search_option(driver)
-            sys.stdout.write(f"\r\n   3    \r\n")
             other_film_sleep_time = other_film_sleep_time * random.uniform(0.9, 1.1)
-            sys.stdout.write(f"\r\n   4    \r\n")
             time.sleep(other_film_sleep_time)
-
-            sys.stdout.write(f"\r\n   other_film_sleep_time: {other_film_sleep_time}\r\n")
-            sys.stdout.write(f"\r\n   hard_will___confidence_to_watch: {hard_will___confidence_to_watch}\n")
 
             if random.randrange(0, 100) < hard_will___confidence_to_watch:
                 title_film, time_film = get_title_time(data_dict, i)
