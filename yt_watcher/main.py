ca89265abf56c07d0a793d423c1870c1e0c51ffa
Piotr Kubon_devC <pkubon2@gmail.com>
2023-06-18 23:53:51 +0200

    watcher changes

--- yt_watcher/main.py
+++ yt_watcher/main.py
@@ -20,15 +20,17 @@ def main(confidenceee, other_film_sleep_time):
     with open(file_name) as json_file:
         data_dict = json.load(json_file)
 
-    sys.stdout.write(f"\r\nother_film_sleep_time: {other_film_sleep_time}\r\n")
-    sys.stdout.write(f"\r\nhard_will___confidence_to_watch: {hard_will___confidence_to_watch}\n")
     try:
         driver = get_init_driver()
         for i in data_dict:
             title_film = ""
             search_for_my_video_in_youtube_search(driver, other_devops_films)
             go_to_rand_film_from_search_option(driver)
-            time.sleep(other_film_sleep_time * random.uniform(0.9, 1.1))
+            other_film_sleep_time = other_film_sleep_time * random.uniform(0.9, 1.1)
+            time.sleep(other_film_sleep_time)
+
+            sys.stdout.write(f"\r\n   other_film_sleep_time: {other_film_sleep_time}\r\n")
+            sys.stdout.write(f"\r\n   hard_will___confidence_to_watch: {hard_will___confidence_to_watch}\n")
 
             if random.randrange(0, 100) < hard_will___confidence_to_watch:
                 title_film, time_film = get_title_time(data_dict, i)
